e7658a7fd5cc3f488af05845d501bd9f20371556
Stefan Bodoarca <stef@stef>
2021-07-07 09:39:42 +0300

    not in production

--- src/main/java/com/ro/model/Constants.java
+++ src/main/java/com/ro/model/Constants.java
@@ -1,7 +1,7 @@
 package com.ro.model;
 
 public final class Constants {
-    public static final boolean PRODUCTION = true;
+    public static final boolean PRODUCTION = false;
 
     private Constants(){};
 }
